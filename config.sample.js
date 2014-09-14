2fec9200befd5983206dffabf04650e58de1dba9
robotanarchy <robotanarchy@users.noreply.github.com>
2014-09-14 21:07:07 +0200

    improve sample config comments

--- config.sample.js
+++ config.sample.js
@@ -1,7 +1,10 @@
 exports.port = 1234;
+
+// put this on the same partition as the imported
+// music collection to avoid unnecessary copying
 exports.tempdir = "./tempdir";
 
-
-// when installed via pip, you may use this instead:
+// when beets was installed via pip, you may use
+// something like this instead:
 // exports.binary = "/home/beets/.local/bin/beet";
 exports.binary = "beet";
