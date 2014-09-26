3fe39d4ffd92dc74e383fda77c082021e3edde71
robotanarchy <robotanarchy@bingo-ev.de>
2014-09-26 12:48:44 +0200

    add beetwerk.sh script, that finds the nodejs binary (even on Debian) and executes beetwerk

---
+++ beetwerk.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get the directory of the script file,even if it was symlinked by npm
+# src: http://stackoverflow.com/a/246128
+# resolve $src until the file is no longer a symlink
+src="${BASH_SOURCE[0]}"
+while [ -h "$src" ]; do
+	dir="$( cd -P "$( dirname "$src" )" && pwd )"
+	src="$(readlink "$src")"
+
+	# if $src was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	[[ $src != /* ]] && src="$dir/$src"
+done
+dir="$( cd -P "$( dirname "$src" )" && pwd )"
+
+
+# Find the nodejs binary. It is usually called 'node', except on Debian,
+# where it is called 'nodejs'.
+node=nodejs
+command -v nodejs >/dev/null 2>&1 || node=node
+
+
+# Start beetwerk
+cd "$dir"
+$node beetwerk.js
